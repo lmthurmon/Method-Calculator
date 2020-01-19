b9bbc34d876ba0b3174c20f225e9055b66b93d60
Landon Thurmon <landonthurmon1@gmail.com>
2020-01-18 19:37:58 -0600

    method calc prac

---
+++ Main Calc/Method Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Method_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number");
+            MyFirstNumber();
+            Console.WriteLine("Enter your second number");
+            MySecondNumber();
+            Console.WriteLine("Choose one of the listed operators:");
+
+        }
+
+        public static double MyFirstNumber()
+        {
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            
+            return firstNumber;
+            
+        }
+
+        public static double MySecondNumber()
+        {
+         
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            return secondNumber;
+
+        }
+    }
+}
